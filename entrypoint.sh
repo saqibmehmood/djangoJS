cd309a0f370c64f5bdd2b978d2f85201980634e4
saqib_mehmood <saqib.mehmood@tkxel.io>
2023-10-20 18:15:09 +0500

    nginx conf

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,20 +6,6 @@ if [ "$APP_INSTANCE" = "web1" ]; then
     # Start the first instance
     python manage.py runserver 0.0.0.0:8000
 
-
-elif [ "$APP_INSTANCE" = "web2" ]; then
-    # Start the first instance
-    python manage.py runserver 0.0.0.0:8002
-
-elif [ "$APP_INSTANCE" = "web3" ]; then
-    # Start the first instance
-    python manage.py runserver 0.0.0.0:8003
-
-elif [ "$APP_INSTANCE" = "web4" ]; then
-    # Start the first instance
-    python manage.py runserver 0.0.0.0:8004
-
-
 else
     echo "Unknown application instance"
     exit 1

7a5b33a5cc646bce43e985d9ef2ae647bc3fbc84
saqib_mehmood <saqib.mehmood@tkxel.io>
2023-10-26 19:08:05 +0500

    postgres

--- Blog/settings.py
+++ Blog/settings.py
@@ -95,11 +95,12 @@ REST_FRAMEWORK = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'djangoJs',  # Change to your database name
+        # 'NAME': 'djangoJs',  # Change to your database name
+        'NAME': 'django-learning',  # Change to your database name
         'USER': 'postgres',  # Change to your database user
-        'PASSWORD': 'postgres123',  # Change to your database password
+        'PASSWORD': 'SBacm7VwUf7Fu2ROVfrM',  # Change to your database password
         # 'HOST': 'db',  # This is the Docker network hostname for the database container
-        'HOST': 'localhost',  # This is the Docker network hostname for the database container
+        'HOST': 'database-1.cbsy9abwsldj.us-east-2.rds.amazonaws.com',  # This is the Docker network hostname for the database container
         # 'HOST': 'web.docker.internal',  # This is the Docker network hostname for the database container
         # 'PORT': '5433',  # Default PostgreSQL port
         'PORT': '5432',  # Default PostgreSQL port

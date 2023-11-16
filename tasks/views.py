39a2a9ebae728e404531f662386cd19f42074f7c
saqib_mehmood <saqib.mehmood@tkxel.io>
2023-11-16 16:52:47 +0500

    cicd

--- tasks/views.py
+++ tasks/views.py
@@ -49,7 +49,7 @@ class Testing2(APIView):
 class Testing3(APIView):
     def get(self, request):
         try:
-            return Response({"message": "If you see this code the congrats you have implemented CICD"}, status=status.HTTP_201_CREATED)
+            return Response({"message": "If you see this code the congrats you have implemented CICD."}, status=status.HTTP_201_CREATED)
         except Exception as e:
             return Response({"message": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
